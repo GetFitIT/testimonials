1f245648f9c463da7790441e347b6f5021242ccd
Prince Shammah <shammahprinz@gmail.com>
2023-10-11 14:34:09 +0100

    initial commit

---
+++ slider.js
@@ -0,0 +1,20 @@
+$('.testi7').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    navText: ['<i class="icon-arrow-left-circle"></i>', '<i class="icon-arrow-right-circle"></i>'],
+    dots: false,
+    autoplay: true,
+    center: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+  
+      },
+      1650: {
+        items: 1
+      }
+    }
+  })
+  
