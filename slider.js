d23ecd3d6cae0013cbb51f2b35f416e16b1d4e34
Prince Shammah <shammahprinz@gmail.com>
2023-10-12 14:38:42 +0100

    fix: make adjustments to testimonials

--- slider.js
+++ slider.js
@@ -1,10 +1,6 @@
 $('.testi7').owlCarousel({
-    loop: true,
     margin: 30,
-    nav: true,
-    navText: ['<i class="icon-arrow-left-circle"></i>', '<i class="icon-arrow-right-circle"></i>'],
     dots: false,
-    autoplay: true,
     center: true,
     responsiveClass: true,
     responsive: {
